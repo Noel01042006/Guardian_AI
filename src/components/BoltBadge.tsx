d3ef75369e78f2d6f047c01499c0540b87795489
Noel Varghese George <noelvgeorge006@gmail.com>
2025-07-02 19:21:01 +0530

    Update BoltBadge.tsx

--- src/components/BoltBadge.tsx
+++ src/components/BoltBadge.tsx
@@ -9,9 +9,9 @@ const CustomBoltBadge: React.FC = () => {
       className="fixed top-4 right-20 z-50"
     >
       <img
-        src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fdev.to%2Fthankamatho%2Fday-3-11fj&psig=AOvVaw0jdq4r7ROiyRlFE9lvq9mH&ust=1751548956361000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCLjrvL2ino4DFQAAAAAdAAAAABAE"
-        alt="Built with Bolt"
-        className="w-32 h-auto"
+        src="/Capture.jpg"
+        alt="Bolt Badge"
+        className="w-20 h-auto"
       />
     </a>
   );
